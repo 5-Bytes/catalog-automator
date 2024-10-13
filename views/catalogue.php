7079109c9072cdacb3a95523887607a0ce16fa17
5-Bytes <simon305martinez@gmail.com>
2024-10-13 19:39:16 -0400

    Inicio del proyecto

---
+++ views/catalogue.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+<head >
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./styles/main.css">
+	<title>Catalogue</title>
+</head>
+<body>
+	<header>
+		<div class="box-logo">
+			<picture>
+				<img  class="logo-grenobil" src="./assets/logo_-grenobil-removebg-preview.png">
+			</picture>
+			<div>
+				<p>Carteras y Bolsos</p>
+			</div>
+		</div>
+		<div class="box-links">
+			<div>
+				<p>¡AL MAYOR A PARTIR DE 3 PIEZAS!</p>
+				<p>Somos Tú Mejor Opción para Invertir</p>
+			</div>
+			<div>
+				<table>
+					<tr>
+						<td>
+							<div>
+								<picture></picture>
+								<p>(+58) 412-7020626</p>
+							</div>
+							<picture>
+								<img src="">
+							</picture>	
+						</td>
+						<td>
+							<div>
+								<picture></picture>
+								<p>@distribuidora_grenobil2022</p>
+							</div>
+							<picture>
+								<img src="">
+							</picture>	
+						</td>
+						<td>
+							<div>
+								<picture></picture>
+								<p>distribuidoragrenobil</p>
+							</div>
+							<picture>
+								<img src="">
+							</picture>	
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</header>
+	<table>
+		
+	</table>
+	<footer>
+		<div>
+			<p>Contamos con mas de <span style="color:red">5000 MODELOS</span> diferentes en Carteras, Bolsos, Bandoleros, Monederos, Koalas, Billeteras, Pañaleras, Pecheras, Musleras, entre otras Variedades de Productos en Variedad de Colores según el modelo: (Negro, Marron, Azul, Rosado, Morado, Rojo, Gris, Vinotinto).</p>
+		</div>
+		<div>
+			<picture>
+				<img class="logo-maps" src="./assets/maps-and-flags.png">
+			</picture>
+			<div>
+				<p>Dirección 1: Calle Sur 13, edif. Residencias Mirador, la Candelaria, Caracas Distrito Capital. / Telf:0424-272-3154</p>
+				<p>Direccion 2: Av. Fuerzas Armadas, Edif. Caribe Piso PB, Local 5, Urb. Catedral, Caracas Distrito Capital / Telf: 0424-269-2396</p>
+			</div>
+		</div>
+		<div class="box-delivery">
+			<picture class="picture-cart">
+				<img class="logo-cart" src="assets/shopping-cart.png">
+			</picture>
+			<div class="text-delivery">
+				<p>CONTAMOS CON DELIVERY</p>
+			</div>
+			<picture class="picture-delivery">
+				<img class="logo-delivery" src="./assets/food-delivery.png">
+			</picture>
+		</div>
+	</footer>
+</body>
+</html>
